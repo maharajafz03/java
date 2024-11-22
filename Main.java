4302da5ca10d3ddbcf297a574d82d47fb814c8ca
maharajafz03 <maharajafz1999@gmail.com>
2024-11-22 20:56:42 +0530

    [java_thing]

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome");
+
+        int number = 32;
+        String name = "hello_world";
+        System.out.println(name);
     }
 }
